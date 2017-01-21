42e48de73540f8b3318248d30314e88e3eff94ac
pco2699 <pco2699@ks-macbook-pro.local>
2017-01-21 13:40:33 +0900

    TEST

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from argparse import ArgumentParser
 
 from flask import Flask, request, abort
 from linebot import (
-    LineBotApi, WebhookParser,  WebhookHandler
+    LineBotApi, WebhookParser
 )
 from linebot.exceptions import (
     InvalidSignatureError
@@ -31,12 +31,12 @@ from linebot.models import (
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
+
 app = Flask(__name__)
 
-PORT = os.getenv('PORT', 8000)
 # get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('CHANNEL_SECRET', None)
-channel_access_token = os.getenv('CHANNEL_ACCESS_TOKEN', None)
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
@@ -46,7 +46,6 @@ if channel_access_token is None:
 
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
-handler = WebhookHandler(channel_secret)
 
 
 @app.route("/callback", methods=['POST'])
@@ -77,18 +76,12 @@ def callback():
 
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
-def message_text(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser(
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
-    arg_parser.add_argument('-p', '--port', default=PORT, help='port')
+    arg_parser.add_argument('-p', '--port', default=8000, help='port')
     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
     options = arg_parser.parse_args()
 
